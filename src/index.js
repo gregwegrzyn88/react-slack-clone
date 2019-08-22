1f5979b4fba260f6546c67f73ec3cbc5fac425c4
Greg Wegrzynowicz <gregwegrzyn88@gmail.com>
2019-08-22 14:25:49 -0500

    made user available globally

--- src/index.js
+++ src/index.js
@@ -15,11 +15,20 @@ import {
   withRouter
 } from "react-router-dom";
 
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser } from "./actions/index";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        console.log(user);
+        // console.log(user);
+        this.props.setUser(user);
         this.props.history.push("/");
       }
     });
@@ -36,12 +45,19 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
+);
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
