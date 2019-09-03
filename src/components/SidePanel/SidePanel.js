3a5b9d01da9a6463ba22895812ca8904e2a018b3
Greg Wegrzynowicz <gregwegrzyn88@gmail.com>
2019-09-03 09:47:11 -0500

    add channel modal and component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,10 +1,12 @@
 import React from "react";
 import UserPanel from "./UserPanel";
+import Channels from "./Channels";
 import { Menu } from "semantic-ui-react";
 
 class SidePanel extends React.Component {
   render() {
     const { currentUser } = this.props;
+
     return (
       <Menu
         size="large"
@@ -13,7 +15,8 @@ class SidePanel extends React.Component {
         vertical
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
-        <UserPanel currentUser= {currentUser}/>
+        <UserPanel currentUser={currentUser} />
+        <Channels />
       </Menu>
     );
   }
