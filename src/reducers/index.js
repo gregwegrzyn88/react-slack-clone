152bf58ae65e06fc76643562ca34d32ffd3a3dc2
Greg Wegrzynowicz <gregwegrzyn88@gmail.com>
2019-10-16 13:46:23 -0500

    color change function added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -25,6 +25,7 @@ const user_reducer = (state = initialUserState, action) => {
 
 const initialChannelState = {
   currentChannel: null,
+
   userPosts: null
 };
 
@@ -35,7 +36,6 @@ const channel_reducer = (state = initialChannelState, action) => {
         ...state,
         currentChannel: action.payload.currentChannel
       };
-      return
     case actionTypes.SET_USER_POSTS:
       return {
         ...state,
@@ -46,9 +46,27 @@ const channel_reducer = (state = initialChannelState, action) => {
   }
 };
 
+const initialColorsState = {
+  primaryColor: "#4c3c4c",
+  secondaryColor: "#eee"
+};
+
+const colors_reducer = (state = initialColorsState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_COLORS:
+      return {
+        primaryColor: action.payload.primaryColor,
+        secondaryColor: action.payload.secondaryColor
+      };
+    default:
+      return state;
+  }
+};
+
 const rootReducer = combineReducers({
   user: user_reducer,
-  channel: channel_reducer
+  channel: channel_reducer,
+  colors: colors_reducer
 });
 
 export default rootReducer;
