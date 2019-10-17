ce4fcc4382379ceef2798fceb8454b1631878c8d
Greg Wegrzynowicz <gregwegrzyn88@gmail.com>
2019-10-16 19:09:45 -0500

    added rules and deployed to google firebase

---
+++ src/components/HomePage/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+class Home extends React.Component {
+  
+  render() {
+    
+    return (
+      <h3>Hello World</h3>
+    )
+  }
+}
+
+export default Home;
